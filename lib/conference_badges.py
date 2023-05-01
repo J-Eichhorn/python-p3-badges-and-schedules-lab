4a161db1af7ad9db4441223d7f7b656dd07f14c1
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-01 03:35:16 -0400

    passes tests

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badges = [f'Hello, my name is {name}.' for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    room = 1
+    list = []
+    for name in names:
+        list.append(f"Hello, {name}! You'll be assigned to room {room}!")
+        room += 1
+    return list
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
